5ac4b3e1e805c13a09db92e798c1691d272818c4
QuangAnhP <qap@sfu.ca>
2024-03-27 16:23:43 -0700

    changed name for easier terminal usage

---
+++ Part1/Queue.cpp
@@ -0,0 +1,77 @@
+/* 
+ * Queue.cpp - recycled from Assignment 2
+ *
+ * Description: Array-based implementation of Queue as an ADT class
+ * Class Invariant: Queue maintained in FIFO order
+ *
+ * Author: Quang Anh Pham
+ * Date: March 2024
+ */
+
+#include "Queue.h"
+#include "EmptyDataCollectionException.h"
+#include <iostream>
+
+
+template<typename ElementType>
+Queue<ElementType>::Queue() : elementCount(0), capacity(10), frontindex(0), backindex(0) {
+    elements = new ElementType[capacity];         //! arbitrary value
+}
+
+template<typename ElementType>
+Queue<ElementType>::Queue(const Queue& aQueue) : elementCount(aQueue.elementCount), capacity(aQueue.capacity), frontindex(aQueue.frontindex), backindex(aQueue.backindex) {
+    elements = new ElementType[capacity];
+    for (unsigned int i = 0; i < elementCount; i++) 
+        elements[i] = aQueue.elements[i];
+}
+
+template<typename ElementType>
+Queue<ElementType>::~Queue() {
+    delete[] elements;
+}
+
+template<typename ElementType>
+bool Queue<ElementType>::isEmpty() const {
+    return (elementCount == 0);
+}
+
+template<typename ElementType>
+void Queue<ElementType>::resize() {
+    //* Create bigger array and copy elements there for more space
+    ElementType* newEle = new ElementType[capacity*2];
+
+    for (unsigned int i = 0; i < elementCount; i++) 
+        newEle[i] = elements[(frontindex + i) % capacity];
+
+    //* Delete old array, assign new array, reset front and back indexes
+    delete[] elements;
+    elements = newEle;
+    this->capacity *= 2;
+    frontindex = 0;
+    backindex = elementCount;
+}
+
+template<typename ElementType>
+bool Queue<ElementType>::enqueue(ElementType& newElement) {
+    if (frontindex == backindex && elementCount != 0)
+        resize();
+
+    elements[backindex] = newElement;
+    backindex = (backindex + 1) % capacity;
+    elementCount++;
+    return true;
+}
+
+template<typename ElementType>
+void Queue<ElementType>::dequeue() {
+    if (elementCount == 0)
+        throw EmptyDataCollectionException("Queue is empty, dequeue fail.");
+
+    frontindex = (frontindex + 1) % capacity;
+    elementCount--;
+}
+
+template<typename ElementType>
+ElementType& Queue<ElementType>::peek() const {
+    return elements[frontindex];    
+}
---
+++ Part1/Queue.h
@@ -0,0 +1,64 @@
+/* 
+ * Queue.h - recycled from Assignment 2
+ *
+ * Description: Array-based implementation of Queue as an ADT class
+ * Class Invariant: Queue maintained in FIFO order
+ *
+ * Author: Quang Anh Pham
+ * Date: March 2024
+ */
+
+#ifndef QUEUE_H
+#define QUEUE_H
+
+template <typename ElementType>
+class Queue {	
+    private:
+        ElementType* elements;                
+        unsigned int elementCount;
+        unsigned int capacity;
+        unsigned int frontindex;
+        unsigned int backindex;
+
+        Queue();
+        Queue(const Queue& aQueue);
+        ~Queue();
+        resize();
+    
+    public:
+/******* Start of Queue Public Interface *******/
+		
+    // Class Invariant:  FIFO or LILO order
+
+    // Description: Returns true if this Queue is empty, otherwise false.
+    // Postcondition: This Queue is unchanged by this operation.
+    // Time Efficiency: O(1)
+    bool isEmpty() const;
+
+    // Description: Inserts newElement at the "back" of this Queue 
+    //              (not necessarily the "back" of this Queue's data structure) 
+    //              and returns true if successful, otherwise false.
+    // Time Efficiency: O(1)
+    bool enqueue(ElementType & newElement);
+
+    // Description: Removes (but does not return) the element at the "front" of this Queue 
+    //              (not necessarily the "front" of this Queue's data structure).
+    // Precondition: This Queue is not empty.
+    // Exception: Throws EmptyDataCollectionException if this Queue is empty.   
+    // Time Efficiency: O(1)
+    void dequeue(); 
+
+    // Description: Returns (but does not remove) the element at the "front" of this Queue
+    //              (not necessarily the "front" of this Queue's data structure).
+    // Precondition: This Queue is not empty.
+    // Postcondition: This Queue is unchanged by this operation.
+    // Exception: Throws EmptyDataCollectionException if this Queue is empty.
+    // Time Efficiency: O(1)
+    ElementType& peek() const;
+
+/******* End of Queue Public Interface *******/
+
+};
+#endif
+
+
